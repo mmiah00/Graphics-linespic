637267734b0c2aaebd068bc1f7c4a64e63e44e72
mmiah00 <mmiah00@stuy.edu>
2020-02-10 23:36:29 +0000

    added line function

--- draw.py
+++ draw.py
@@ -1,4 +1,20 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    pass
+    slope = (y1 - y0) / (x1 - x0) + 0.0
+    if slope > 0 & slope < 1:
+        octant1 (x0, y0, x1, y1, color)
+    elif slope
+
+def octant1 (x0, y0, x1, y1, color):
+    x,y = x1, y1
+    A = y2 - y1
+    B = -1 * (x2 - x1)
+    d = 2*A + B
+    while x < x1:
+        plot (x,y, color)
+        if d > 0:
+            y = y + 1
+            d = d + 2*B
+        x = x + 1
+        d = d + 2*A
