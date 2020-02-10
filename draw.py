aeb11cbbddf9edda468bfe05b56c7835c97bb2be
mmiah00 <mmiah00@stuy.edu>
2020-02-10 23:49:37 +0000

    .

--- draw.py
+++ draw.py
@@ -1,18 +1,18 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    slope = (y1 - y0) / (x1 - x0) + 0.0
-    if slope > 0 & slope < 1:
-        octant1 (x0, y0, x1, y1, color)
-    elif slope
+    if x1 - x0 != 0:
+        slope = ((y1 - y0) / (x1 - x0)) + 0.0
+        if (slope > 0) & (slope < 1):
+            octant1 (x0, y0, x1, y1, screen, color)
 
-def octant1 (x0, y0, x1, y1, color):
-    x,y = x1, y1
-    A = y2 - y1
-    B = -1 * (x2 - x1)
+def octant1 (x0, y0, x1, y1, screen, color):
+    x,y = x0, y0
+    A = y1 - y0
+    B = -1 * (x1 - x0)
     d = 2*A + B
     while x < x1:
-        plot (x,y, color)
+        plot (screen, color, x, y)
         if d > 0:
             y = y + 1
             d = d + 2*B
--- line.py
+++ line.py
@@ -3,18 +3,19 @@ pic = open ("linepic.ppm", "w")
 pic.write ("P3 500 500 255 ")
 
 YELLOW = "255 255 0"
+BLACK = "0 0 0"
 
-def initialize (background_color):
+def background (background_color):
     for y in range (500):
         for x in range (500):
             pixels.append (background_color.split (" "))
 
-initialize (YELLOW)
+background (BLACK)
 
 def plot (x, y, color):
-    pixels[x][y] = color.split (" ")
+    pixels[x]= color.split (" ")
 
-def line (x1, y1, x2, y2, color):
+def octant1 (x1, y1, x2, y2, color):
     x,y = x1, y1
     A = y2 - y1
     B = -1 * (x2 - x1)
@@ -27,7 +28,7 @@ def line (x1, y1, x2, y2, color):
         x = x + 1
         d = d + 2*A
 
-line (50, 50, 150, 100, "0 0 0")
+octant1 (50,50,150,100, YELLOW)
 
 def drawpic ():
     for y in range (500):
