665d61fd6df075a1aa6c98f34b41d76495d48e58
mmiah00 <mmiah00@stuy.edu>
2020-02-11 01:29:26 +0000

    octant 3 and 4

--- draw.py
+++ draw.py
@@ -7,8 +7,10 @@ def draw_line( x0, y0, x1, y1, screen, color ):
             octant1 (x0, y0, x1, y1, screen, color)
         elif slope > 1:
             octant2 (x0, y0, x1, y1, screen, color)
+        elif slope < -1:
+            octant3 (x1, y1, x0, y0, screen, color)
         elif (slope < 0) & (slope > -1):
-            octant1 (x1, y1, x0, y0, screen, color) #octant4 
+            octant4 (x1, y1, x0, y0, screen, color)
 
 def octant1 (x0, y0, x1, y1, screen, color): #also for octant 5
     x,y = x0, y0
@@ -35,3 +37,29 @@ def octant2 (x0, y0, x1, y1, screen, color): #also for octant 6
             d = d + 2*A
         y = y + 1
         d = d + 2*B
+
+def octant3 (x0, y0, x1, y1, screen, color): #also for octant 7
+    x,y = x0, y0
+    A = y1 - y0
+    B = -1 * (x1 - x0)
+    d = A - 2*B
+    while y >= y1:
+        plot (screen, color, x, y)
+        if d > 0:
+            x = x + 1
+            d = d + 2*A
+        y = y - 1
+        d = d + 2*B
+
+def octant4 (x0, y0, x1, y1, screen, color): #also for octant 8
+    x,y = x0, y0
+    A = y1 - y0
+    B = -1 * (x1 - x0)
+    d = 2*A - B
+    while x >= x1:
+        plot (screen, color, x, y)
+        if d < 0:
+            y = y - 1
+            d = d - 2*B
+        x = x + 1
+        d = d + 2*A
