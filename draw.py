793492b4a7d0db37c1faa648cbc9f8788b99db4a
mmiah00 <mmiah00@stuy.edu>
2020-02-11 01:51:06 +0000

    fixeD?

--- draw.py
+++ draw.py
@@ -7,10 +7,10 @@ def draw_line( x0, y0, x1, y1, screen, color ):
             octant1 (x0, y0, x1, y1, screen, color)
         elif slope > 1:
             octant2 (x0, y0, x1, y1, screen, color)
-        # elif slope < -1:
-        #     octant3 (x0, y0, x1, y1, screen, color)
-        # elif (slope < 0) & (slope > -1):
-        #     octant4 (x0, y0, x1, y1, screen, color)
+        elif slope < -1:
+            octant3 (x0, y0, x1, y1, screen, color)
+        elif (slope < 0) & (slope > -1):
+            octant4 (x0, y0, x1, y1, screen, color)
 
 
 def octant1 (x0, y0, x1, y1, screen, color): #also for octant 5
@@ -50,14 +50,14 @@ def octant3 (x0, y0, x1, y1, screen, color): #also for octant 7
             x = x + 1
             d = d + 2*A
         y = y - 1
-        d = d + 2*B
+        d = d - 2*B
 
 def octant4 (x0, y0, x1, y1, screen, color): #also for octant 8
     x,y = x0, y0
     A = y1 - y0
     B = -1 * (x1 - x0)
     d = 2*A - B
-    while x >= x1:
+    while x <= x1:
         plot (screen, color, x, y)
         if d < 0:
             y = y - 1
