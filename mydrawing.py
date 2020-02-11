8d604626781d3c54ada1106c7ec4339bf72b9662
mmiah00 <mmiah00@stuy.edu>
2020-02-11 03:17:54 +0000

    making my darwing

---
+++ mydrawing.py
@@ -0,0 +1,43 @@
+from display import *
+from draw import *
+
+s = new_screen()
+YELLOW = [255, 255, 0]
+BLACK = [0, 0, 0]
+
+draw_line (50, 435, 50 + 90, 425 -120, s, YELLOW)
+
+# #octants 1 and 5
+# draw_line(0, 0, XRES-1, YRES-1, s, c)
+# draw_line(0, 0, XRES-1, YRES / 2, s, c)
+# draw_line(XRES-1, YRES-1, 0, YRES / 2, s, c)
+#
+# #octants 8 and 4
+# c[BLUE] = 255;
+# draw_line(0, YRES-1, XRES-1, 0, s, c);
+# draw_line(0, YRES-1, XRES-1, YRES/2, s, c);
+# draw_line(XRES-1, 0, 0, YRES/2, s, c);
+#
+# #octants 2 and 6
+# c[RED] = 255;
+# c[GREEN] = 0;
+# c[BLUE] = 0;
+# draw_line(0, 0, XRES/2, YRES-1, s, c);
+# draw_line(XRES-1, YRES-1, XRES/2, 0, s, c);
+#
+# #octants 7 and 3
+# c[BLUE] = 255;
+# draw_line(0, YRES-1, XRES/2, 0, s, c);
+# draw_line(XRES-1, 0, XRES/2, YRES-1, s, c);
+#
+# #horizontal and vertical
+# c[BLUE] = 0;
+# c[GREEN] = 255;
+# draw_line(0, YRES/2, XRES-1, YRES/2, s, c);
+# draw_line(XRES/2, 0, XRES/2, YRES-1, s, c);
+
+
+display(s)
+save_ppm(s, 'binary2.ppm')
+save_ppm_ascii(s, 'ascii2.ppm')
+save_extension(s, 'drawing.png')
