68f00fe407452ca808c053ef86113b0b4ae79a7d
mmiah00 <mmiah00@stuy.edu>
2020-02-11 03:27:03 +0000

    trying to draw rectangle

--- mydrawing.py
+++ mydrawing.py
@@ -6,8 +6,9 @@ YELLOW = [255, 255, 0]
 BLACK = [0, 0, 0]
 
 draw_line (50, 425, 50 + 90, 425 -120, s, YELLOW)
-draw_line (50 + 90, 425 -120, 50, 435, s, YELLOW)
-
+draw_line (50, 425, 50 + 40, 425 + 30, s, YELLOW)
+draw_line (50 + 90, 425 - 120, 50 + 90 + 30, 425 - 120 + 40, s, YELLOW)
+draw_line (50 + 90 + 30, 425 - 120 + 40, 50 + 40, 425 + 30, s YELLOW)
 # #octants 1 and 5
 # draw_line(0, 0, XRES-1, YRES-1, s, c)
 # draw_line(0, 0, XRES-1, YRES / 2, s, c)
